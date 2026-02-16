bf8da71d706880eee3f86a75b265b2cba08bf0cb
Lita Beach <litabeach123@gmail.com>
2026-02-16 11:55:23 -0600

    backend added

---
+++ environments/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "${local.prefix}-rg-01"
+    storage_account_name = "${local.restricted_prefix}-sa-01"
+    container_name       = "dev-blob-container"
+    key                  = "dev.tfstate"
+  }
+}
